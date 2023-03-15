f20c0bdcffcfbaa4ec49f60a323208c72100baa9
Focus <maniaclord@bk.ru>
2023-03-16 01:22:16 +0300

    data transform with deprecated onActivityResult

--- android/app/src/main/java/com/example/secretgenerator/MainActivity.kt
+++ android/app/src/main/java/com/example/secretgenerator/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.secretgenerator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var publicKeyTextView: TextView
@@ -30,15 +33,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         moveButton.setOnClickListener {
-            val intent = Intent(this@MainActivity, SecretActivity::class.java)
-            intent.putExtra("publicKey", publicKeyTextView.text.toString())
-            startActivityForResult(intent, REQUEST_SECRET_KEY)
+            if (publicKeyTextView.text.isNotEmpty()) {
+                val intent = Intent(this@MainActivity, SecretActivity::class.java)
+                intent.putExtra(KEY, publicKeyTextView.text.toString())
+                startActivityForResult(intent, REQUEST_SECRET_KEY)
+                publicKeyTextView.text = ""
+            } else
+                Toast.makeText(this@MainActivity, "YOU SHOULD GENERATE KEY", Toast.LENGTH_SHORT)
+                    .show()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_SECRET_KEY -> {
+                    val key = data?.getStringExtra(KEY)
+                    Toast.makeText(this@MainActivity, key, Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "ERROR: WRONG KEY PROVIDED",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- android/app/src/main/java/com/example/secretgenerator/SecretActivity.kt
+++ android/app/src/main/java/com/example/secretgenerator/SecretActivity.kt
@@ -1,16 +1,40 @@
 package com.example.secretgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+
+const val KEY = "${R.string.app_package_name}.KEY"
 
 class SecretActivity : AppCompatActivity() {
-
+    private lateinit var checkTextInput: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secret)
 
-        val publicKey = intent.getStringExtra("publicKey")
+        checkTextInput = findViewById(R.id.checkTextInput)
+        val checkButton: Button = findViewById(R.id.checkButton)
 
+        val publicKey = intent.getStringExtra(KEY)
+
+        checkButton.setOnClickListener {
+            val answerIntent = Intent()
+
+            if (checkTextInput.text.isNullOrEmpty())
+                return@setOnClickListener
+
+            if (checkTextInput.text.toString() == publicKey) {
+                answerIntent.putExtra(KEY, "XDDDD")
+                setResult(RESULT_OK, answerIntent)
+            } else
+                setResult(RESULT_CANCELED, answerIntent)
+
+            finish()
+        }
 
     }
 
