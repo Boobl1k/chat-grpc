e0550ef059e3c668da987612fc35e73f3b7c077b
Focus <maniaclord@bk.ru>
2023-03-16 00:35:59 +0300

    fixed styles and constraints

--- android/app/src/main/java/com/example/secretgenerator/MainActivity.kt
+++ android/app/src/main/java/com/example/secretgenerator/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.example.secretgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import java.util.Date
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var keyTextView: TextView
+    private lateinit var publicKeyTextView: TextView
 
     private fun generatePublicKey() {
 
+    }
+
+    private companion object {
+        const val REQUEST_SECRET_KEY = 0
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        keyTextView = findViewById(R.id.keyTextView)
+        publicKeyTextView = findViewById(R.id.keyTextView)
         val generateButton: Button = findViewById(R.id.generateButton)
+        val moveButton: Button = findViewById(R.id.moveButton)
 
         generateButton.setOnClickListener {
-            keyTextView.text = "amgous))"
+            publicKeyTextView.text = "amgous))"
         }
+
+        moveButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecretActivity::class.java)
+            intent.putExtra("publicKey", publicKeyTextView.text.toString())
+            startActivityForResult(intent, REQUEST_SECRET_KEY)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
 
     }
 }
---
+++ android/app/src/main/java/com/example/secretgenerator/SecretActivity.kt
@@ -0,0 +1,17 @@
+package com.example.secretgenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecretActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secret)
+
+        val publicKey = intent.getStringExtra("publicKey")
+
+
+    }
+
+}
